82404b4f944804054bd2b8a010a56b38d09af393
Aspen Yoo <ay963@login-0-1.local>
2016-08-23 12:57:02 -0400

    fits

--- analysis/4_realdata/VSTM_fitparam.sh
+++ analysis/4_realdata/VSTM_fitparam.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1
-#PBS -l walltime=10:00:00
+#PBS -l walltime=8:00:00
 #PBS -j oe
 #PBS -M aspen.yoo@nyu.edu
-#PBS -l mem=6GB
+#PBS -l mem=4GB
 #PBS -m abe
-#PBS -N VSTM_paramfit
+#PBS -N changedetection
 
 index=${PBS_ARRAYID}
 job=${PBS_JOBID}
@@ -16,8 +16,8 @@ module load matlab
 
 export MATLABPATH=/home/ay963/matlab-scripts
 cat<<EOF | matlab -nodisplay
-addpath('/home/ay963/job-scripts')
 addpath(genpath('/home/ay963/matlab-scripts'))
+addpath(genpath('/home/ay963/changedetection'))
 
 subjids = {'1','2','3','4','5'};
 blah = num2str($index);
