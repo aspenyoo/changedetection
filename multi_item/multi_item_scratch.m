bc3a98a6ca404cff75da3ab439fef38b6602c108
aspenyoo <aspen.yoo@nyu.edu>
2019-06-03 10:16:39 -0400

    blobface

--- multi_item/multi_item_scratch.m
+++ multi_item/multi_item_scratch.m
@@ -201,12 +201,14 @@ runmax = 1;
 
 clear all
 
-filename = 'analysis/clusterfittingsettings.mat';
+% filename = 'analysis/clusterfittingsettings.mat';
+filename = 'analysis/clusterfitting_joint.mat';
 % filename = 'analysis/clusterjobs_keshvari.mat';
+additionalpaths = ''; %'ellipse_keshvari/'
 
-subjidCell = {'S91','S92','S93','S94','S95','S96','S97','S98','S99'};
-% subjidCell = {'S02','S03','S06','S07','S08','S10','S11','S14','S04'};
-conditionCell = {'Ellipse'};
+% subjidCell = {'S91','S92','S93','S94','S95','S96','S97','S98','S99'};
+subjidCell = {'S02','S03','S06','S07','S08','S10','S11','S14','S04'};
+conditionCell = {'combined'};
 modelMat = ...
     [1 1 1;  1 2 1; ...  % V_O model variants
      1 1 2;  1 2 2; ...  % V_M model variants
@@ -232,7 +234,7 @@ for isubj = 1:nSubj
         for imodel = 1:nModels
             model = modelMat(imodel,:);
             try
-            load(sprintf('analysis/fits/ellipse_keshvari/subj%s_%s_model%d%d%d.mat',subjid,condition,model(1),model(2),model(3)));
+            load(sprintf('analysis/fits/%ssubj%s_%s_model%d%d%d.mat',additionalpaths,subjid,condition,model(1),model(2),model(3)));
             
             % get the indices of runlists left
             incompleteRuns = 1:20;
@@ -304,11 +306,11 @@ end
 
 clear all
 
-% subjidVec = {'S02','S03','S06','S08','S10','S11','S14'};
-subjidVec = {'S91','S92','S93','S94','S95','S96','S97','S98','S99'};
-condition = 'Ellipse';
-additionalpaths = 'ellipse_keshvari/'; % ellipse_keshvari
-additionalmodifier = '_keshvari';
+subjidVec = {'S02','S03','S06','S08','S10','S11','S14'};
+% subjidVec = {'S91','S92','S93','S94','S95','S96','S97','S98','S99'};
+condition = 'combined';
+additionalpaths = '';%'ellipse_keshvari/'; % ellipse_keshvari
+additionalmodifier = '';%'_keshvari';
 
 % modelMat = [1 1 1; 1 1 2; 1 3 1; 1 3 2];
 modelMat = ...
@@ -357,7 +359,7 @@ save(sprintf('analysis/fits/bfp_%s%s.mat',condition,additionalmodifier),'LLMat',
 
 clear all
 
-condition = 'Ellipse_keshvari';
+condition = 'combined';
 load(sprintf('analysis/fits/bfp_%s.mat',condition));
 % foldername = 'ellipse_keshvari';
 % load(sprintf('analysis/fits/%s/bfp_%s.mat',foldername,condition));
@@ -679,7 +681,7 @@ condition = 'combined';
 % disptype = 'same';
 subjidx = 4;
 modelidx = 1;
-nBins = 6;
+nBins = 8;
 nSamples = 200;
 
 % get correct settings
@@ -729,7 +731,7 @@ modelMat = [1 1 1; 1 1 2; 1 3 1; 1 3 2];
 nSubj = length(subjidVec);
 nModels = size(modelMat,1);
 
-model = modelMat(modelidx,:);         
+model = modelMat(imodel,:);         
 
 % load ML parameter estimates
 load(sprintf('analysis/fits/bfp_%s.mat',condition))
@@ -760,10 +762,8 @@ for isubj = 1:nSubj
     [LL,p_C_hat] = calculate_joint_LL(bfp,data_E,data_L,model,[],nSamples);
     fprintf('subj %s: %5.2f \n',subjid,LL)
 
-    figure(1);
     [x_mean_e(:,:,isubj), pc_data_e(:,:,isubj), pc_pred_e(:,:,isubj)] = plot_psychometric_fn(data_E,nBins,p_C_hat.Ellipse,0);
     [x_mean_l(:,:,isubj), pc_data_l(:,:,isubj), pc_pred_l(:,:,isubj)] = plot_psychometric_fn(data_L,nBins,p_C_hat.Line,0);
-    pause;
 end
 
 % get participant and model means
@@ -800,7 +800,7 @@ for ii = 1:5;
 plot_summaryfit(xrange_l(ii,:),partM_l(ii,:),partSEM_l(ii,:),modelM_l(ii,:),...
     modelSEM_l(ii,:),colorMat(ii,:),colorMat(ii,:))
 end
-title('Ellipse')
+title('Line')
 defaultplot
 
 
