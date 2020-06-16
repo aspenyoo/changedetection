0f92f2443e2a1b1aff588a8007ce28e7b7a44f9e
aspenyoo <aspen.yoo@nyu.edu>
2020-06-16 16:35:23 -0400

    first subj model

--- multi_item/analysis/recalc_LL.s
+++ multi_item/analysis/recalc_LL.s
@@ -2,12 +2,12 @@
 #
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=04:00:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=6GB
 #SBATCH --job-name=fit_parameters
 #SBATCH --mail-type=END
 #SBATCH --mail-user=aspen.yoo@nyu.edu
-#SBATCH --output=fp_%a.out
+#SBATCH --output=llcalc_%a.out
 
 module purge
 module load matlab/2016b
